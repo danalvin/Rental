618495542d98b57a4708bf7f27bb772bf1982e4d
danalvin <mungaiwaituika@gmail.com>
2023-03-20 15:16:57 +0300

    writing tests for the apps

--- occupation/tests.py
+++ occupation/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Occupation
+from datetime import date, timedelta
+
+
+class OccupationModelTestCase(TestCase):
+    def setUp(self):
+        Occupation.objects.create(
+            tenant_name="John Doe",
+            house_name="House 1",
+            move_in_date=date.today() - timedelta(days=30),
+            rent_amount=15000,
+            rent_due_date=date.today() + timedelta(days=7)
+        )
+
+    def test_occupation_creation(self):
+        occupation = Occupation.objects.get(tenant_name="John Doe")
+        self.assertEqual(str(occupation), "John Doe - House 1")
+        
+    def test_occupation_list_view(self):
+        response = self.client.get(reverse('occupation_list'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_occupation_detail_view(self):
+        occupation = Occupation.objects.get(tenant_name="John Doe")
+        response = self.client.get(reverse('occupation_detail', args=[occupation.id]))
+        self.assertEqual(response.status_code, 200)
--- tenant/tests.py
+++ tenant/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Tenant
+
+class TenantModelTestCase(TestCase):
+    def setUp(self):
+        Tenant.objects.create(
+            first_name="John",
+            last_name="Doe",
+            email="johndoe@example.com",
+            phone_number="1234567890"
+        )
+
+    def test_tenant_creation(self):
+        tenant = Tenant.objects.get(first_name="John")
+        self.assertEqual(str(tenant), "John Doe")
+        
+    def test_tenant_list_view(self):
+        response = self.client.get(reverse('tenant_list'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_tenant_detail_view(self):
+        tenant = Tenant.objects.get(first_name="John")
+        response = self.client.get(reverse('tenant_detail', args=[tenant.id]))
+        self.assertEqual(response.status_code, 200)
